c84b5efe10c94677e07b37c506e6cc5837735b3d
BlockMaster <145277345+ProgrammProfi@users.noreply.github.com>
2025-04-12 19:00:51 +0300

    Add Fighters and Wizards, attack mechanic.

--- logic.py
+++ logic.py
@@ -15,6 +15,17 @@ class Pokemon:
         self.front_img = self.get_front_img()
         self.back_img = self.get_back_img()
         self.name = self.get_name()
+        if randint(0, 500) == 0:
+            self.is_super = True
+        else:
+            self.is_super = False
+        self.normal_hp = randint(10, 50)
+        self.normal_power = randint(1, 5)
+        if self.is_super:
+            self.normal_hp += 20
+            self.normal_power += 2
+        self.current_hp = self.normal_hp
+        self.current_power = self.normal_power
         Pokemon.pokemons[pokemon_trainer] = self
     
     
@@ -50,9 +61,21 @@ class Pokemon:
             return "Pikachu"
     
     
+    def attack(self, enemy):
+        if enemy.current_hp > self.current_power:
+            if isinstance(enemy, Wizard):
+                if randint(1, 5) == 1:
+                    return "Покемон-волшебник применил щит в сражении"
+            enemy.current_hp -= self.current_power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.current_hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    
+    
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покемона: {self.name}, его класс - {"Волшебник" if isinstance(self, Wizard) else "Боец"}, он {"Редкий" if self.is_super else "Обычный"}"
     
     
     # Метод класса для получения картинки покемона
@@ -62,3 +85,25 @@ class Pokemon:
     
     def show_back_img(self):
         return self.back_img
+
+
+class Wizard(Pokemon):
+    def __init__(self, pokemon_trainer_):
+        super().__init__(pokemon_trainer_)
+        self.normal_hp += 10
+        self.current_hp = self.normal_hp
+
+
+class Fighter(Pokemon):
+    def __init__(self, pokemon_trainer_):
+        super().__init__(pokemon_trainer_)
+        self.normal_power += 3
+        self.current_power = self.normal_power
+    
+    
+    def attack(self, enemy):
+        super_power = randint(1, 3)
+        self.current_power += super_power
+        result = super().attack(enemy)
+        self.current_power -= self.normal_power
+        return result + f"Боец применил супер-атаку силой: {super_power}"
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from random import randint
 import telebot
 from config import TOKEN
 
-from logic import Pokemon
+from logic import Fighter, Pokemon, Wizard
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -12,19 +12,20 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def go(message):
     if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
+        if randint(1, 2) == 1:
+            pokemon = Fighter(message.from_user.username)
+        else:
+            pokemon = Wizard(message.from_user.username)
         bot.send_message(message.chat.id, pokemon.info())
         bot.send_photo(message.chat.id, pokemon.show_front_img())
         bot.send_photo(message.chat.id, pokemon.show_back_img())
     else:
         bot.reply_to(message, "Ты уже создал себе покемона")
-    if randint(0, 500) == 0:
-        bot.send_message(message.chat.id, "Ты получил супер-покемона! Твой промокод: QT4FX5R. Используй /promo твой_промокод чтобы активировать его")
 
 
 @bot.message_handler(commands=['promo'])
 def promo(message):
     if message.text.split()[1] == "QT4FX5R":
-        bot.send_message(message.chat.id, "Как ты смог получить это сообщение? Шанс этого 1 к 1000!!!")
+        bot.send_message(message.chat.id, "Как ты смог получить это сообщение?")
 
 bot.infinity_polling(none_stop=True)
